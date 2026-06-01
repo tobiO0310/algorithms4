98c4bce240e6f43bc1ebe98ab0c6f42eeccb0011
Toby <59401633+tobiO0310@users.noreply.github.com>
2026-06-01 19:37:30 +0200

    feat:finish elementary sorts

---
+++ src/sorting/elementary.rs
@@ -0,0 +1,196 @@
+use super::is_sorted;
+
+/// Sorts the array in-place, using elementary sort.
+///
+/// In the worst case, this makes ~ &frac12; <em>n</em><sup>2</sup> compares
+/// and ~ &frac12; <em>n</em><sup>2</sup> exchanges to sort an array of length <em>n</em>.
+/// On average, both of the aforementioned values are halved.
+///
+/// It is stable and uses &Theta;(1) extra space (not including input array).
+pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 1..n {
+        let mut j = i;
+        while j > 0 && arr[j] < arr[j - 1] {
+            arr.swap(j, j - 1);
+            j -= 1;
+        }
+
+        debug_assert!(is_sorted(arr, 0, i));
+    }
+    debug_assert!(is_sorted(arr, 0, n));
+}
+
+/// Sorts the array in-place, using an optimized version of elementary sort.
+/// This uses half exchanges, instead of full exchanges, and a sentinel.
+///
+/// In the worst case, this makes ~ &frac12; <em>n</em><sup>2</sup> compares to sort an array of length <em>n</em>.
+/// On average, both of the aforementioned values are halved.
+///
+/// It is stable and uses &Theta;(1) extra space (not including input array).
+pub fn insertion_sort_x<T: Clone + Ord>(arr: &mut [T]) {
+    let n = arr.len();
+
+    // set i=0 to be sentinel
+    let mut exchanges = 0;
+    for i in (1..n).rev() {
+        if arr[i] < arr[i - 1] {
+            arr.swap(i, i - 1);
+            exchanges += 1;
+        }
+    }
+    if exchanges == 0 {
+        return;
+    }
+
+    // elementary sort with half-exchanges
+    for i in 2..n {
+        let v = arr[i].clone();
+
+        let mut j = i;
+        while v < arr[j - 1] {
+            arr[j] = arr[j - 1].clone();
+            j -= 1;
+        }
+        arr[j] = v;
+    }
+
+    debug_assert!(is_sorted(arr, 0, n));
+}
+
+/// Sorts the array in-place, using binary search and insertion sort with half exchanges.
+///
+/// In the worst case, this makes ~ &frac12; <em>n</em><sup>2</sup> exchanges to sort an array of length <em>n</em>.
+/// On average, both of the aforementioned values are halved.
+///
+/// It is stable and uses &Theta;(1) extra space (not including input array).
+pub fn binary_insertion_sort<T: Clone + Ord>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 1..n {
+        // binary search in the sorted area
+        let v = arr[i].clone();
+        let mut lo = 0;
+        let mut hi = i;
+        while lo < hi {
+            let mid = lo + (hi - lo) / 2;
+            if v < arr[mid] {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        // insertion sort with half exchanges
+        for j in (lo + 1..=i).rev() {
+            arr[j] = arr[j - 1].clone();
+        }
+        arr[lo] = v;
+    }
+
+    debug_assert!(is_sorted(arr, 0, n));
+}
+
+/// Sorts the array in-place, using selection sort.
+///
+/// This implementation makes ~ &frac12; <em>n</em><sup>2</sup> exchanges to sort an array of length <em>n</em>.
+/// It performs exactly <em>n</em> exchanges.
+///
+/// It is stable and uses &Theta;(1) extra space (not including input array).
+pub fn selection_sort<T: Ord>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 0..n {
+        let mut min = i;
+        for j in i + 1..n {
+            if arr[j] < arr[min] {
+                min = j;
+            }
+        }
+        arr.swap(i, min);
+        debug_assert!(is_sorted(arr, 0, i));
+    }
+    debug_assert!(is_sorted(arr, 0, n));
+}
+
+/// Sorts the array in-place, using Shellsort with
+/// <a href = "https://oeis.org/A003462"> Knuth's increment sequence</a>
+/// (1, 4, 13, 40, ...). In the worst case, this implementation makes
+/// &Theta;(<em>n</em><sup>3/2</sup>) compares and exchanges to sort
+/// an array of length <em>n</em>.
+///
+/// This sorting algorithm is not stable.
+/// It uses &Theta;(1) extra memory (not including the input array).
+pub fn shell_sort<T: Ord>(arr: &mut [T]) {
+    let n = arr.len();
+
+    // get the highest usable number in the Knuth's increment sequence
+    let mut increment = 1;
+    while increment < n / 3 {
+        increment = 3 * increment + 1;
+    }
+
+    while increment >= 1 {
+        // h-sort the array
+        for i in increment..n {
+            let mut j = i;
+            while j >= increment && arr[j] < arr[j - increment] {
+                arr.swap(j, j - increment);
+                j -= 1;
+            }
+        }
+        if cfg!(debug_assertions) {
+            // only run in non-release state
+            for i in increment..n {
+                if arr[i] < arr[i - increment] {
+                    panic!("did not sort correctly")
+                }
+            }
+        }
+        increment /= 3;
+    }
+    debug_assert!(is_sorted(arr, 0, n));
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::prelude::*;
+
+    use super::*;
+
+    macro_rules! test_sort {
+        ($($name:ident),*) => ($(
+            paste::paste! {
+                #[test]
+                fn [< $name _works >]() {
+                    let mut arr = vec![4, 5, 2, 6, 3, 1];
+                    $name(&mut arr);
+                    assert_eq!(arr, vec![1, 2, 3, 4, 5, 6]);
+                }
+
+                #[test]
+                fn [< $name _big_works >]() {
+                    let mut rand = rand::rng();
+
+                    let mut arr = vec![0; 100];
+                    for item in arr.iter_mut() {
+                        *item = rand.random_range(0..1000);
+                    }
+
+                    let mut clone = arr.to_vec();
+                    clone.sort(); // assume rust's sorting works
+
+                    insertion_sort(&mut arr);
+
+                    assert_eq!(arr, clone);
+                }
+            }
+        )*);
+    }
+
+    test_sort!(
+        insertion_sort,
+        insertion_sort_x,
+        binary_insertion_sort,
+        selection_sort,
+        shell_sort
+    );
+}
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,5 +1,17 @@
 //! This module holds all the different sorting algorithms described in chapter 2 of the book.
 
-mod insertion;
+/// Test if array is actually sorted
+#[cfg(debug_assertions)]
+#[inline] // it is fine to inline when only run during non-release
+fn is_sorted<T: Ord>(arr: &[T], lo: usize, hi: usize) -> bool {
+    for i in (lo + 1)..hi {
+        if arr[i] < arr[i - 1] {
+            return false;
+        }
+    }
+    true
+}
 
-pub use insertion::insertion_sort;
+mod elementary;
+
+pub use elementary::*;
