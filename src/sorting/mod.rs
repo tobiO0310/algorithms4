648e5b78e2f1f9fd70d53ed5494c5d4917934596
Toby <59401633+tobiO0310@users.noreply.github.com>
2026-06-09 11:01:41 +0200

    feat:update CI
    Latest dependencies is managed by dependabot on GitHub. Furthermore, now
    fails if clippy emits a warning or rustfmt was not run. (must use
    nightly, since config uses unstable features)

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -34,70 +34,70 @@ fn is_sorted<T: Ord>(arr: &[T]) -> bool {
 
 #[cfg(test)]
 macro_rules! test_sort {
-        ($($name:ident),*) => ($(
-            pastey::paste! {
-                #[test]
-                fn [<$name _works>]() {
-                    let mut arr = vec![4, 5, 2, 6, 3, 1];
-                    $name(&mut arr);
-                    assert!(is_sorted(&arr));
-                    assert_eq!(arr, vec![1, 2, 3, 4, 5, 6]);
-                }
-
-                #[test]
-                fn [<$name _big_works>]() {
-                    let mut rand = rand::rng();
-
-                    let mut arr = vec![0; 100];
-                    for item in arr.iter_mut() {
-                        *item = rand.random_range(0..1000);
-                    }
-
-                    let mut clone = arr.to_vec();
-                    clone.sort(); // assume rust's sorting works
-
-                    $name(&mut arr);
-
-                    assert!(is_sorted(&arr));
-                    assert_eq!(arr, clone);
-                }
-
-                #[test]
-                fn [<$name _really_big_works>]() {
-                    let mut rand = rand::rng();
-
-                    let mut arr = vec![0; 10_000];
-                    for item in arr.iter_mut() {
-                        *item = rand.random_range(0..1000);
-                    }
-
-                    let mut clone = arr.to_vec();
-                    clone.sort(); // assume rust's sorting works
-
-                    $name(&mut arr);
-
-                    assert!(is_sorted(&arr));
-                    assert_eq!(arr, clone);
-                }
-
-                #[test]
-                fn [<$name _single>]() {
-                    let mut arr = vec![1];
-                    $name(&mut arr);
-                    assert!(is_sorted(&arr));
-                    assert_eq!(arr, vec![1]);
-                }
-
-                #[test]
-                fn [<$name _pre_sorted>]() {
-                    let mut arr = vec![1, 4, 5, 9, 14, 42];
-                    $name(&mut arr);
-                    assert!(is_sorted(&arr));
-                    assert_eq!(arr, vec![1, 4, 5, 9, 14, 42]);
-                }
+    ($($name:ident),*) => ($(
+        pastey::paste! {
+            #[test]
+            fn [<$name _works>]() {
+                let mut arr = vec![4, 5, 2, 6, 3, 1];
+                $name(&mut arr);
+                assert!(is_sorted(&arr));
+                assert_eq!(arr, vec![1, 2, 3, 4, 5, 6]);
             }
-        )*);
-    }
+
+            #[test]
+            fn [<$name _big_works>]() {
+                let mut rand = rand::rng();
+
+                let mut arr = vec![0; 100];
+                for item in arr.iter_mut() {
+                    *item = rand.random_range(0..1000);
+                }
+
+                let mut clone = arr.to_vec();
+                clone.sort(); // assume rust's sorting works
+
+                $name(&mut arr);
+
+                assert!(is_sorted(&arr));
+                assert_eq!(arr, clone);
+            }
+
+            #[test]
+            fn [<$name _really_big_works>]() {
+                let mut rand = rand::rng();
+
+                let mut arr = vec![0; 10_000];
+                for item in arr.iter_mut() {
+                    *item = rand.random_range(0..1000);
+                }
+
+                let mut clone = arr.to_vec();
+                clone.sort(); // assume rust's sorting works
+
+                $name(&mut arr);
+
+                assert!(is_sorted(&arr));
+                assert_eq!(arr, clone);
+            }
+
+            #[test]
+            fn [<$name _single>]() {
+                let mut arr = vec![1];
+                $name(&mut arr);
+                assert!(is_sorted(&arr));
+                assert_eq!(arr, vec![1]);
+            }
+
+            #[test]
+            fn [<$name _pre_sorted>]() {
+                let mut arr = vec![1, 4, 5, 9, 14, 42];
+                $name(&mut arr);
+                assert!(is_sorted(&arr));
+                assert_eq!(arr, vec![1, 4, 5, 9, 14, 42]);
+            }
+        }
+    )*);
+}
 
 mod elementary;
 mod merge;
